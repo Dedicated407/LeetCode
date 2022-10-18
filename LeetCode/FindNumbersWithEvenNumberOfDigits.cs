95347f5260aad723a5bf44aeb7c3919486711f77
Tsypin Ilya <ilyaboy@list.ru>
2022-10-18 13:44:05 +0300

    Add Find Numbers with Even Number of Digits (topic - arrays)

---
+++ LeetCode/FindNumbersWithEvenNumberOfDigits.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode;
+
+public static class FindNumbersWithEvenNumberOfDigits
+{
+    public static int FindNumbers(int[] nums)
+    {
+        var counter = 0;
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i].ToString().Length % 2 == 0)
+            {
+                counter++;
+            }
+        }
+
+        GC.Collect();
+        
+        return counter;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,7 @@
 ﻿using LeetCode;
 
-var res = MaxConsecutiveOnes.FindMaxConsecutiveOnes(new[] {1,1,0,1,1,1});
-Console.WriteLine(res);
+var res1 = MaxConsecutiveOnes.FindMaxConsecutiveOnes(new[] {1,1,0,1,1,1});
+Console.WriteLine(res1);
+
+var res2 = FindNumbersWithEvenNumberOfDigits.FindNumbers(new[] {12,345,2,6,7896});
+Console.WriteLine(res2);
