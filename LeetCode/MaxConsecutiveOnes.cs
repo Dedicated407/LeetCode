0d0b5822f1c4aba87ab7170cc4392dd01ea78109
Tsypin Ilya <ilyaboy@list.ru>
2022-10-18 13:34:48 +0300

    Add Max consecutive ones task (topic - arrays)

---
+++ LeetCode/MaxConsecutiveOnes.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode;
+
+public static class MaxConsecutiveOnes
+{
+    public static int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int maxCount = 0;
+        int count = 0;
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                count++;
+                
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+
+        return maxCount;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello world!");
+﻿using LeetCode;
+
+var res = MaxConsecutiveOnes.FindMaxConsecutiveOnes(new[] {1,1,0,1,1,1});
+Console.WriteLine(res);
