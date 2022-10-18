b535d69988cf15c9bc919c2d9af7475e4be0b3b8
Tsypin Ilya <ilyaboy@list.ru>
2022-10-18 13:59:37 +0300

    Add Squares of a Sorted Array (topic - arrays)

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,7 +1,13 @@
 ﻿using LeetCode;
 
-var res1 = MaxConsecutiveOnes.FindMaxConsecutiveOnes(new[] {1,1,0,1,1,1});
+var res1 = MaxConsecutiveOnes.FindMaxConsecutiveOnes(new[] {1, 1, 0, 1, 1, 1});
 Console.WriteLine(res1);
 
-var res2 = FindNumbersWithEvenNumberOfDigits.FindNumbers(new[] {12,345,2,6,7896});
+var res2 = FindNumbersWithEvenNumberOfDigits.FindNumbers(new[] {12, 345, 2, 6, 7896});
 Console.WriteLine(res2);
+
+var res3 = SquaresOfSortedArray.SortedSquares(new[] {-4, -1, 0, 3, 10});
+for (var i = 0; i < res3.Length; i++)
+{
+    Console.Write($"{res3[i]} ");
+}
---
+++ LeetCode/SquaresOfSortedArray.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode;
+
+public static class SquaresOfSortedArray
+{
+    /// <summary>
+    /// Given an integer array nums sorted in non-decreasing order.
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns>an array of the squares of each number sorted in non-decreasing order</returns>
+    public static int[] SortedSquares(int[] nums)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            nums[i] *= nums[i];
+        }
+        
+        Array.Sort(nums);
+        
+        GC.Collect();
+
+        return nums;
+    }
+    
+    // Other solution
+    // public static int[] SortedSquares(int[] nums) =>
+    //     nums.Select(x => x * x).OrderBy(x => x).ToArray();
+}
