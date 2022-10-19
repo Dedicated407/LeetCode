52fec4628ba3f98e5bd371602ecee34a0fe8868b
Tsypin Ilya <ilyaboy@list.ru>
2022-10-19 18:39:34 +0300

    Add Remove duplicates from sorted array (topic - arrays)

---
+++ LeetCode/Arrays/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Arrays;
+
+public static class RemoveDuplicatesFromSortedArray
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int counter = 0;
+        
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[counter] < nums[i])
+            {
+                counter++;
+                nums[counter] = nums[i];
+            }
+        }
+
+        return counter + 1;
+    }
+}
---
+++ UnitTests/Arrays/RemoveDuplicatesFromSortedArrayUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Arrays;
+using Xunit;
+
+namespace UnitTests.Arrays;
+
+public class RemoveDuplicatesFromSortedArrayUnitTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var res1 = RemoveDuplicatesFromSortedArray.RemoveDuplicates(new[] {1, 1, 2});
+        var res2 = RemoveDuplicatesFromSortedArray.RemoveDuplicates(new[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4});
+        var res3 = RemoveDuplicatesFromSortedArray.RemoveDuplicates(new[] {1});
+        var res4 = RemoveDuplicatesFromSortedArray.RemoveDuplicates(new[] {3, 3});
+
+        Assert.Equal(2, res1);
+        Assert.Equal(5, res2);
+        Assert.Equal(1, res3);
+        Assert.Equal(1, res4);
+    }
+}
