067ffe71894196e3331118d2c63dd63fc3d979d2
Tsypin Ilya <ilyaboy@list.ru>
2022-10-20 19:20:18 +0300

    Add Valid Mountain Array (arrays)

---
+++ LeetCode/Arrays/ValidMountainArrayTask.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Arrays;
+
+public static class ValidMountainArrayTask
+{
+    public static bool ValidMountainArray(int[] arr)
+    {
+        if (arr.Length < 3)
+        {
+            return false;
+        }
+
+        bool increase;
+
+        if (arr[0] < arr[1])
+        {
+            increase = true;
+        }
+        else
+        {
+            return false;
+        }
+
+        for (var i = 1; i < arr.Length - 1; i++)
+        {
+            if (increase)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    increase = false;
+                }
+
+                if (arr[i] == arr[i + 1])
+                {
+                    return false;
+                }
+            } 
+            else
+            {
+                if (arr[i] <= arr[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return !increase;
+    }
+}
---
+++ UnitTests/Arrays/ValidMountainArrayUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using LeetCode.Arrays;
+using Xunit;
+
+namespace UnitTests.Arrays;
+
+[MemoryDiagnoser]
+[RankColumn]
+public class ValidMountainArrayUnitTest
+{
+    [Fact]
+    [Benchmark]
+    public void Test1()
+    {
+        var res1 = ValidMountainArrayTask.ValidMountainArray(new[] {2, 1});
+        var res2 = ValidMountainArrayTask.ValidMountainArray(new[] {3, 5, 5});
+        var res3 = ValidMountainArrayTask.ValidMountainArray(new[] {0, 3, 2, 1});
+        var res4 = ValidMountainArrayTask.ValidMountainArray(new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+        var res5 = ValidMountainArrayTask.ValidMountainArray(new[] {9, 8, 7, 6, 5});
+
+        Assert.False(res1);
+        Assert.False(res2);
+        Assert.True(res3);
+        Assert.False(res4);
+        Assert.False(res5);
+    }
+}
